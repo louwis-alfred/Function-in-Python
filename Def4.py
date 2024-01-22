a8ea9899034dbaf33772379283c6d24964d044da
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-22 17:46:34 +0800

    Add files via upload

--- Def4.py
+++ Def4.py
@@ -1,6 +1,8 @@
-def hello(to="World"):
-    print("Hello, " + to)
-
-hello()
-name = input("What's your name? ").strip().title()
-hello(name)
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        print(number)
+        total = total * number
+    return total
+    
+print(multiply(2,3,4,5,6,7))
