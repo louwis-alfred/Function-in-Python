92610600ac42493b209bd456f3cafe2428dfe006
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-22 17:29:19 +0800

    Add files via upload

---
+++ Def.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print(f"Hi, {name}")
+
+greet("Louwis")
+
+    
+# def get_greeting(name):
+#     return f"Hi {name}"
+
+# message = get_greeting("Louwis")
+# print(message)
---
+++ Def3.py
@@ -0,0 +1,8 @@
+def greet(name):
+    print(f"Hi, {name}")
+    
+def get_greeting(name):
+    return f"Hello {name}"
+
+message = get_greeting("Louwis")
+print(message)
