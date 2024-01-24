b41ce57a6ee81e5c5f2f58c6f4f69eb20c201b97
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-24 09:17:18 +0800

    Add files via upload

---
+++ Def8.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    z = x + y
+    return z
+
+def subtract (x, y):
+    z = x - y
+    return z
+
+def multiply (x, y):
+    z = x * y
+    return z
+
+print(add(1,2))
+print(subtract(43, 32))
