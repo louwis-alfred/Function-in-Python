a7e4e51c564a56abe36e90008073ef8f3d0c800e
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-29 21:33:51 +0800

    Update and rename Def2.py to SimpleGreet2.py

--- SimpleGreet2.py
+++ SimpleGreet2.py
@@ -2,6 +2,6 @@ def greet(first_name, last_name):
     print(f"Hi {first_name} {last_name}")
     print("Welcome aboard")
     
-greet("Louwis", "Alfred")
+greet("Elon", "Musk")
 print()
 greet("John", "Cena")
