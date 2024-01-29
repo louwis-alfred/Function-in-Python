f2265493be8a3fc0fa411bd89a63b616d98faa0d
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-29 21:32:16 +0800

    Update Def.py

--- Def.py
+++ Def.py
@@ -1,11 +1,11 @@
 def greet(name):
     print(f"Hi, {name}")
 
-greet("Louwis")
+greet("yozora")
 
     
 # def get_greeting(name):
 #     return f"Hi {name}"
 
-# message = get_greeting("Louwis")
+# message = get_greeting("yozora")
 # print(message)
